067881d5feba6ed03a5c180c014166ce27f3b982
Kirk Mendoza <kirkmendoza@gmail.com>
2025-03-11 23:25:56 -0500

    Commented out testing code.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const order = prompt(
     "Please enter all your desired froyo flavors using a comma to separate them.",
-    "vanilla,banana,Banana,oreo,vanilla,chocolate,vanilla,vanilla,oreo,strawberry,oreo,coffee,catnip,dognip,coffee,coffee,chilli,butter pecan,strawberry,butter pecan,coffee,coffee,toffee"
+    "Vanilla,banana,Banana,Oreo,Vanilla,chocolate,vanilla,vanilla,oreo,strawberry,oreo,coffee,catnip,dognip,coffee,coffee,chilli,butter pecan,strawberry,butter pecan,coffee,coffee,toffee"
 )
 
 let orderStats = {}
@@ -10,8 +10,8 @@ let orderStats = {}
 function startOrder(myFlavors) {
     const flavors = myFlavors.split(",")
     for ( let i = 0 ; i < flavors.length ; i++ ){
-        console.log(i)
-        console.log(flavors[i].toLowerCase())
+        // console.log(i)
+        // console.log(flavors[i].toLowerCase())
         if (orderStats[flavors[i].toLowerCase()] == undefined){
             orderStats[flavors[i].toLowerCase()] = 0
         }
